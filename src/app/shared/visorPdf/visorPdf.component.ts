8035bba8eed623a64d4a70282e71bcb667828bfc
cesarurbina <curbina@unitru.edu.pe>
2023-01-05 09:48:15 -0500

    "Grado"

---
+++ src/app/shared/visorPdf/visorPdf.component.ts
@@ -0,0 +1,54 @@
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef, Inject, Input, OnDestroy, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Subject } from 'rxjs';
+
+@Component({
+    selector: 'visor-dialog',
+    templateUrl: './visorPdf.component.html',
+    styles: [
+        `
+            pdf-viewer {
+                height: 65vh;
+                width: auto;
+            }
+        `
+    ]
+})
+export class VisorPdfComponent implements OnInit, OnDestroy {
+    //@Input() isEdite: boolean = false;
+    //@Output() onDelete: EventEmitter<any> = new EventEmitter<any>();
+    page: number = 1;
+    pdfSource: any;
+    // Private
+    //pdfSource  = "Voucher.pdf";
+    private _unsubscribeAll: Subject<any> = new Subject<any>();
+    constructor(
+        @Inject(MAT_DIALOG_DATA) public data: any,
+        public dialogRef: MatDialogRef<VisorPdfComponent>,
+    ) {}
+
+    ngOnInit(): void {
+        this.verArchivo();
+    }
+    ngOnDestroy(): void {
+        // Unsubscribe from all subscriptions
+        this._unsubscribeAll.complete();
+    }
+    verArchivo(): void {
+        console.log(this.data.archivoPdf);
+        const file = this.data.archivoPdf;
+        if (file.type === 'application/pdf') {
+            const reader = new FileReader();
+            reader.readAsDataURL(file);
+            reader.onload = (e) => {
+                this.pdfSource = e.target.result;
+            };
+        }
+        else{
+            this.pdfSource = file;
+            console.log(this.pdfSource);
+        }
+        //reader.readAsArrayBuffer(this.data.file);
+    }
+}
